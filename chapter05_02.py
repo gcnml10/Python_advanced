3d07992e2a577d7c4cae81a2cb16d0e11c07e4c3
gcnml10 <gskm77037@naver.com>
2020-11-18 18:21:46 +0900

    iter

--- chapter05_02.py
+++ chapter05_02.py
@@ -85,10 +85,134 @@ def repeat_outer(obj):
         del obj.a
     return repeat_inner
 
-print(min(timeit.repeat(repeat_outer(use_slot), number=100000)))
-print(min(timeit.repeat(repeat_outer(no_slot), number=100000)))
+# print(min(timeit.repeat(repeat_outer(use_slot), number=100000)))
+# print(min(timeit.repeat(repeat_outer(no_slot), number=100000)))
 
 print()
 print()
 
 # 객체 슬라이싱
+
+class ObjectS:
+    def __init__(self):
+        self._numbers = [n for n in range(1,10000,3)]
+
+    def __len__(self):
+        return len(self._numbers)
+
+    def __getitem__(self, idx):
+        return self._numbers[idx]
+
+s = ObjectS()
+
+# print('EX3-1 -', s.__dict__)
+print('EX3-2 -', len(s))
+print('EX3-2 -', len(s._numbers))
+print('EX3-4 -', s[1:100])
+print('EX3-4 -', s[-1])
+print('EX3-4 -', s[::10])
+
+print()
+print()
+
+# 파이썬 추상 메소드
+
+# 자체적으로 객체 생성 불가
+# 상속을 통해서 자직 클래스에서 인스턴스를 생성해야 함
+# 개발과 관련된 공통된 내용(필드, 메소드) 추출 및 통합해서 공통된 내용으로 작성하게 하는 것
+
+# Sequence 상속 받지 않았지만, 자동으로 기능 작동 __iter__, __contain__(in 연산자) 기능 작동
+# 객체 전체를 자동으로 조사 -> 시퀀스 프로토콜
+
+class IterTestA():
+    def __getitem__(self, idx):
+        return range(1,50,2)[idx] 
+
+i1 = IterTestA()
+
+print('EX4-1 -', i1[4])
+print('EX4-1 -', i1[4:10])
+print('EX4-1 -', 3 in i1[1:10])
+# print('EX4-4 -', [i for i in i1[:]])
+
+print()
+print()
+
+# Sequence 상소
+# 요구사항인 추상 메소드를 모두 구현해야 동장
+
+from collections.abc import Sequence
+class IterTestB(Sequence):          #FM 대로 상속을 받았으니까 FM대로 구현해줘야함
+    def __getitem__(self, idx):
+        return range(1,50,2)[idx] 
+        
+    def __len__(self):
+        return len(range(1,10,2))
+
+i2 = IterTestB()
+print('EX4-1 -', i2[4])
+print('EX4-1 -', i2[4:10])
+print('EX4-1 -', 3 in i2[1:10])
+
+print()
+print()
+
+# abc 활용 예제
+import abc
+
+class RandomMachine(abc.ABC):  # metaclass = abc.ABCMeta(3.4 이하)
+    # __metaclass__ = abc.ABCMeta
+
+    # 추상 메소드
+    @abc.abstractmethod
+    def load(self, iterobj):
+        '''Iterable 항목 추가'''
+    
+    #추상 메소드
+    @abc.abstractmethod
+    def pick(self):
+        '''무작위 항목 뽑기'''
+
+    def inspect(self):
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+            return tuple(sorted(items))
+
+import random
+
+class CraneMachine(RandomMachine):
+    def __init__(self,items):
+        self._randomizer = random.SystemRandom()
+        self._items = []
+        self.load(items)
+
+    def load(self, items):
+        self._items.extend(items)
+        self._randomizer.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('Empty Crane Box')
+
+    def __call__(self):
+        return self.pick()
+
+# 서브 클래스 확인
+print('EX5-1 -', issubclass(RandomMachine, CraneMachine))
+print('EX5-1 -', issubclass(CraneMachine, RandomMachine))
+
+# 상속 구조 확인
+print('EX5-3 -', CraneMachine.__mro__)
+
+cm = CraneMachine(range(1,100)) # 추상 메소드 구현 안하면 에러
+
+print('EX5-4 -', cm._items)
+print('EX5-5 -', cm.pick())
+print('EX5-5 -', cm())
+print('EX5-5 -', cm.inspect())
